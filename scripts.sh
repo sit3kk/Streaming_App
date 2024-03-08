d9c2a24f127e997b1f488a9efd252c7d7dbcfb6c
sit3kk <sito.270@interia.pl>
2024-03-08 23:22:54 +0100

    Startup script

--- scripts.sh
+++ scripts.sh
@@ -2,11 +2,18 @@
 
 
 run() {
-    gnome-terminal -- bash -c "cd djangoproj; source venv/bin/activate; python manage.py runserver; exec bash"
+    #gnome-terminal -- bash -c "cd djangoproj; source venv/bin/activate; python manage.py runserver; exec bash"
 
-    gnome-terminal -- bash -c "cd reactapp; npm run start; exec bash"
+    #gnome-terminal -- bash -c "cd reactapp; npm run start; exec bash"
 
-    gnome-terminal -- bash -c "docker ps -a | grep -q my-redis-container && sudo docker start my-redis-container || sudo docker run --name my-redis-container -d -p 6379:6379 redis; exec bash"
+    #gnome-terminal -- bash -c "docker ps -a | grep -q my-redis-container && sudo docker start my-redis-container || sudo docker run --name my-redis-container -d -p 6379:6379 redis; exec bash"
+
+    
+    gnome-terminal -- bash -c "sudo docker compose up --build; exec bash"
+
+    gnome-terminal --bash -c "cd reactapp; REACT_APP_API_KEY=http://127.0.0.1:8000/api REACT_APP_WS_URL=ws://127.0.0.1:8000/ws yarn build; cd dist; python3 -m http.webserver 3000; exec bash"
+    
+    
 }
 
 run
